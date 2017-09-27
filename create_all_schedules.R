cae2ff80f15e8318c5ae14396a823c5e94342627
Aaron Politsky <aaron.politsky@gmail.com>
2017-09-26 22:18:55 -0500

    parameterizes schedule creation based on number of teams and division size.  renames schedule columns.

--- create_all_schedules.R
+++ create_all_schedules.R
@@ -1,26 +1,26 @@
 require('data.table')
 
-num.teams <- 6
-div.size <- 2
+num.teams <- 12
+div.size <- 4
 
 create.reg.season.schedule.all.teams <- function() {
 
  # import generic schedule matrix for teams assigned 1:num.teams
   if(num.teams==12) {
     schedule.matrix <- as.data.table(read.csv("./ffl schedule - Sheet1.csv", 
-                                              row.names=1))
+                                              row.names=1, colClasses = c("integer")))
   } else if (num.teams==6) {
     schedule.matrix <- as.data.table(read.csv("./six-team schedule - Sheet1.csv", 
-                                              row.names=1))
+                                              row.names=1, colClasses = c("integer")))
   } 
     
   # form a long data.table with week, home, away column
-  schedule.dt <- data.table("week" = numeric(),
-                            "team.id" = integer(),
-                            "opponent.id" = integer())
+  schedule.dt <- data.table("week" = integer(),
+                            "home.slot.id" = integer(),
+                            "opp.slot.id" = integer())
   last.div.play.weeks.dt <- copy(schedule.dt)
   
-  for (tid in as.numeric(rownames(schedule.matrix))) {
+  for (tid in as.integer(rownames(schedule.matrix))) {
     for(w in 1:(num.teams-1)) {
       opp.id <- which(schedule.matrix[tid,]==w)
       schedule.dt <- rbind(schedule.dt, list(w, tid, opp.id))
